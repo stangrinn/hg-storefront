afd38d68ca38793219d7e568166a7961018c4055
Stan Grinshpun <stangrinn@gmail.com>
2025-12-29 14:06:56 +0200

    Feature: Update environment configuration for Docker - Modify apiHost to use API_URL from environment variables - Update apiPort to dynamically parse port from API_URL - Set default values for apiHost and apiPort if API_URL is not defined

--- src/environments/environment.docker.ts
+++ src/environments/environment.docker.ts
@@ -1,7 +1,11 @@
 export const environment = {
     production: true,
-    apiHost: 'http://localhost',
-    apiPort: 3000,
+    apiHost: typeof process !== 'undefined' && process.env?.['API_URL'] 
+        ? process.env['API_URL'].replace(/:\d+$/, '') 
+        : 'http://backend',
+    apiPort: typeof process !== 'undefined' && process.env?.['API_URL']
+        ? parseInt(process.env['API_URL'].match(/:(\d+)$/)?.[1] || '3000')
+        : 3000,
     shopApiPath: 'shop-api',
     baseHref: '/',
     tokenMethod: 'bearer',
